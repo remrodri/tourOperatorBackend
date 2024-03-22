4ddd4327dc6029d9e9c444d2e926fa12ae0d6c36
Rodrigo tucuman <rembert.rodrigo@gmail.com>
2024-03-22 17:59:54 -0400

    se se modifico el campo de name por firstName asi como todos los componentes involucrados

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -1,31 +1,34 @@
-
-const { ObjectId } = require('mongodb');
-const { connectDB, getClient, closeDB,dbName } = require('../services/db');
+const { ObjectId } = require("mongodb");
+const { connectDB, getClient, closeDB, dbName } = require("../services/db");
 
 const getAllUsersDB = async () => {
   try {
     const client = getClient();
     const db = client.db(dbName);
-    const collection = db.collection('users');
+    const collection = db.collection("users");
     const users = await collection.find({}).toArray();
-    return users;
+    const refinedUsers = users.map(user => {
+      const { password, createAt, updateAt, ...newUser } = user;
+      return newUser;
+    })
+    return refinedUsers;
   } catch (error) {
-    console.error('Error al recuperar usuarios', error);
-    throw new Error('Error interno del servidor');
+    console.error("Error al recuperar usuarios", error);
+    throw new Error("Error interno del servidor");
   }
-}
+};
 
-async function getByIdDB(id) { 
+async function getByIdDB(id) {
   try {
     const client = getClient();
     const db = client.db(dbName);
-    const collection = db.collection('users');
+    const collection = db.collection("users");
     const result = await collection.findOne({ _id: new ObjectId(id) });
     //console.log(result);
     return result;
   } catch (error) {
-    console.error('Error al recuperar usuarios', error);
-    throw new Error('Error interno del servidor');
+    console.error("Error al recuperar usuarios", error);
+    throw new Error("Error interno del servidor");
   }
 }
 
@@ -33,12 +36,12 @@ async function createDB(data) {
   try {
     const client = getClient();
     const db = client.db(dbName);
-    const collection = db.collection('users');
+    const collection = db.collection("users");
     const result = await collection.insertOne(data);
     return result;
   } catch (error) {
-    console.error('Error al insertar usuario', error);
-    throw new Error('error interno del servidor');
+    console.error("Error al insertar usuario", error);
+    throw new Error("error interno del servidor");
   }
 }
 
@@ -46,12 +49,15 @@ async function updateDB(id, data) {
   try {
     const client = getClient();
     const db = client.db(dbName);
-    const collection = db.collection('users');
-    const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: data });
+    const collection = db.collection("users");
+    const result = await collection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: data }
+    );
     return result;
   } catch (error) {
-    console.error('Error en la actualización de datos', error);
-    throw new Error('Error interno del servidor');
+    console.error("Error en la actualización de datos", error);
+    throw new Error("Error interno del servidor");
   }
 }
 
@@ -59,12 +65,34 @@ async function removeDB(id) {
   try {
     const client = getClient();
     const db = client.db(dbName);
-    const collection = db.collection('users');
+    const collection = db.collection("users");
     const result = await collection.deleteOne({ _id: new ObjectId(id) });
     return result;
   } catch (error) {
     console.log("Error en la eliminacion del usuario", error);
-    throw new Error('Error interno del servidor');
+    throw new Error("Error interno del servidor");
+  }
+}
+
+async function getUserByParamsDB(userName, password) {
+  try {
+    const client = getClient();
+    const db = client.db(dbName);
+    const collection = db.collection("users");
+    const result = await collection.findOne({
+      userName: userName,
+      password: password,
+    });
+    if (!result) {
+      throw new Error("Usuario o contraseña incorrectos");
+    }
+    delete result.createAt;
+    delete result.password;
+    delete result.updateAt;
+    return result;
+  } catch (error) {
+    console.log("Error al buscar el usuario por parametros", error);
+    throw error;
   }
 }
 module.exports = {
@@ -72,5 +100,6 @@ module.exports = {
   getByIdDB,
   createDB,
   updateDB,
-  removeDB
+  removeDB,
+  getUserByParamsDB,
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,8 @@ const {
   getByIdDB,
   createDB,
   updateDB,
-  removeDB
+  removeDB,
+  getUserByParamsDB
 } = require('../repository/userRepository');
 
 async function getAllUsers() {
@@ -30,10 +31,29 @@ async function remove(id) {
   const result = await removeDB(id);
   return result;
 }
+
+async function getUserByParams(params) {
+  try {
+    const user = await getUserByParamsDB(params.userName, params.password);
+    // delete user.password;
+    // delete user.createAt;
+    //delete user._id;
+    // delete user.updateAt;
+    return user
+  } catch (error) {
+    if (error.message === "Usuario o contraseña incorrectos") {
+      throw new Error("Usuario o contraseña incorrectos");
+    } else {
+      console.error("error al buscar usuario por parametros", error);
+      throw new Error("Error interno del servidor");
+    }
+  }
+}
 module.exports = {
   getAllUsers,
   getById,
   create,
   update,
-  remove
+  remove,
+  getUserByParams
   };
